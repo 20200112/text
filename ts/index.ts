3ba56c5e3532393965c754db1c7b59a6242015ed
lf_egg <ningmengyudan@gmail.com>
2024-05-12 00:43:31 +0800

    自学一阶段学来的（有我当时想弄的网站{内含屎山代码}

---
+++ ts/index.ts
@@ -0,0 +1,17 @@
+class Button{
+    constructor(){
+
+    }
+
+    getHtmlH1(){
+        document.getElementsByClassName('a')
+    }
+
+    getHtmlAudio(){
+        document.getElementsByClassName('b')
+    }
+
+    hiddenH1(){
+        
+    }
+}
